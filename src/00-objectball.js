9407ea27001e1b27e399d233277444037587afb7
Richard <rlew0925@gmail.com>
2025-02-14 18:25:55 -0700

    Update 00-objectball.js

---
+++ src/00-objectball.js
@@ -0,0 +1,58 @@
+function gameObject() {
+    return {
+        home: {
+            teamName: "Brooklyn Nets",
+            colors: ["Black, White"],
+            players: {
+                "Alan Anderson": {
+                    number: 0,
+                    shoe: 16,
+                    points: 22,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 3,
+                    blocks: 1,
+                    "slam dunks": 1
+                },
+                "Reggie Evans": {
+                    number: 30,
+                    shoe: 14,
+                    points: 12,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 12,
+                    blocks: 12,
+                    "slam dunks": 7
+                }
+            }, 
+        },
+        away: {
+            teamName: "Charlotte Hornets",
+            colors: ["Turquoise, Purple"],
+            players: {
+                "Jeff Adrien": {
+                    number: 4,
+                    shoe: 18,
+                    points: 10,
+                    rebounds: 1,
+                    assists: 1,
+                    steals: 2,
+                    blocks: 7,
+                    "slam dunks": 2
+                },
+                "Bismak Biyombo": {
+                    number: 0,
+                    shoe: 16,
+                    points: 12,
+                    rebounds: 4,
+                    assists: 7,
+                    steals: 7,
+                    blocks: 15,
+                    "slam dunks": 10
+                }
+            }
+        }
+    }
+}
+
+console.log(gameObject());
